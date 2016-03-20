01f6983ce385762e74ceab9f8f59a96734c3065f
Olga <olga.bu@core-tech.ru>
2016-03-20 19:58:24 +0300

    Third commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,5 @@ module.exports = {
     output: {
         path: __dirname + '/app',
         filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
     }
 };
